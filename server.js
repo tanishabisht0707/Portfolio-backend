8961a013f4f8bf53a6b63581e3c97089b3ebee51
Tanisha Bisht <145261911+tanishabisht0707@users.noreply.github.com>
2025-12-31 13:12:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 // Contact route
-app.post('/send-email', async (req, res) => {
+app.post('/', async (req, res) => {
   const { name, email, message } = req.body;
 
   if (!name || !email || !message) {
